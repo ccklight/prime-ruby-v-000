56c5cba3f0b83353a7f3e678e72aa9301b74a4e3
Cheryl K <cherylkelson@icloud.com>
2018-09-18 19:06:41 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,20 +2,12 @@ def prime?(number)
   if number == 0 or number == 1 or number < 0
     return false 
   end 
-  i = 2  
-  limit = number / i 
-  while i < limit 
-    if number % i == 0 
-      return false 
-    end 
-  i += 1 
-  limit = number / i 
-  end 
-  return true 
-end
+ 
+ 
+ 
+ 
+ 
 
-
-#     return false
 #   else
 #     (2..num-1).to_a.all? do |possible_factor|
 #       num % possible_factor != 0
